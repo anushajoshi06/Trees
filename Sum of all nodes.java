a2624151b49e61fc3c76ee882b87fd15398347dd
Anusha Joshi <anushajoshi2651@gmail.com>
2019-06-24 00:13:02 +0530

    Add files via upload

---
+++ Sum of all nodes.java
@@ -0,0 +1,41 @@
+/*Sum of all nodes
+
+Given a generic tree, count and return the sum of all nodes present in the given tree.
+Input format :
+Elements in level order form separated by space (as per done in class). Order is - 
+Root_data, n (No_Of_Child_Of_Root), n children, and so on for every element 
+Output Format :
+Sum of all nodes
+Sample Input :
+10 3 20 30 40 2 40 50 0 0 0 0 
+Sample Output :
+190
+*/
+
+
+public class Solution {
+
+/*	TreeNode structure 
+ * 
+ * class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}*/
+	
+	
+	
+	public static int sumOfAllNode(TreeNode<Integer> root){
+	int sumOfAllNode=0;
+
+for(TreeNode<Integer>child:root.children) {
+			//TreeNode<Integer> child = takeInput();
+		sumOfAllNode+=sumOfAllNode(child);	
+		}
+		return sumOfAllNode+root.data;
+	}
+}
